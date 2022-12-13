a31df8a3269b83890e630d03b3aab4ee5165e5be
Lina Petersson <lipe2106@student.miun.se>
2022-12-13 13:20:02 +0100

    Felhantering

--- maui_calculator/MainPage.xaml.cs
+++ maui_calculator/MainPage.xaml.cs
@@ -9,37 +9,54 @@ public partial class MainPage : ContentPage
 
 	private void Calculate(object sender, EventArgs e)
 	{
-		// Get input from entry No1 and entry No2 and convert to int
-		int one = int.Parse(No1.Text);
-		int two = int.Parse(No2.Text);
+		// Get input from entry No1 and entry No2 and convert to int if not empty
+		if (No1.Text == null || No2.Text == null)
+		{
+			Error.Text = "Du måste fylla i siffror!";
+			return;
+		}
+		else
+		{
+			// Convert to int
+			int one = int.Parse(No1.Text);
+			int two = int.Parse(No2.Text);
 
-		// Get selected index from picker and save in variable
-        int selected = Select.SelectedIndex;
+			// Get selected index from picker and save in variable
+			int selected = Select.SelectedIndex;
 
-        //Declare result variable
-        int result;
+			//Declare result variable
+			int result;
 
-		// Control what index user chose and calculate numbers accordingly
-		switch(selected)
-		{ 
-			case 0: result = one + two;
-				Result.Text = $"{result}";
-                break;
-			case 1: result = one - two;
-                Result.Text = $"{result}";
-                break;
-            case 2: result = one * two;
-                Result.Text = $"{result}";
-                break;
-            case 3: result = one / two;
-                Result.Text = $"{result}";
-                break;
-			default: Result.Text =  "Välj räknesätt";
-				break;
-        }
+			// Control what index user chose and calculate numbers accordingly
+			switch (selected)
+			{
+				case 0:
+					result = one + two;
+					Result.Text = $"{result}";
+					Error.Text = "";
+					break;
+				case 1:
+					result = one - two;
+					Result.Text = $"{result}";
+					Error.Text = "";
+					break;
+				case 2:
+					result = one * two;
+					Result.Text = $"{result}";
+					Error.Text = "";
+					break;
+				case 3:
+					result = one / two;
+					Result.Text = $"{result}";
+					Error.Text = "";
+					break;
+				default:
+					Error.Text = "Välj ett räknesätt";
+					break;
+			}
 
-		SemanticScreenReader.Announce(Result.Text);
+			SemanticScreenReader.Announce(Result.Text);
+		}
 	}
-
 }
 
